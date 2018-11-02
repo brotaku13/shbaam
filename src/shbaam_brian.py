0b939b03610fe817532940610bb7f101b91cc5ba
brotaku13 <Brian.caulfield13@gmail.com>
2018-11-02 13:06:50 -0700

    finished off the shbaam_brian. File is ready for integration and testing

--- src/shbaam_brian.py
+++ src/shbaam_brian.py
@@ -1,6 +1,8 @@
 import netCDF4
 import datetime
 import csv
+import subprocess
+import os
 
 """
 Computes total terrestrial water storage anomaly timeseries. 
@@ -64,6 +66,7 @@ return:
     timestrings {list} a list of datetime strings with each date representing a different month in the netCDF4 file
 """
 def create_timestrings(input_netCDF4):
+    print('Determining datestrings...')
     #grab initial date from conc file. this uses the units from the conc file
     timestring = input_netCDF4.variables['time'].split(' ')[-1]
     origin_date = datetime.datetime.strptime(timestring, '%Y-%m-%d %H:%M:%S')
@@ -77,6 +80,7 @@ def create_timestrings(input_netCDF4):
 
     return [date.strftime('%m/%d/%Y') for date in timestrings]
 
+
 """
 Helper function for creating datestrings. Increments the month by one. This can't be done using timedelta 
 because a month is not a uniform measure. 
@@ -86,7 +90,6 @@ input:
 
 return:
     {datetime} an object which is a month ahead of the input datetime
-
 """
 def add_month(date):
     new_year = date.year
@@ -97,3 +100,225 @@ def add_month(date):
     new_day = date.day
     return datetime.datetime(new_year, new_month, new_day)
 
+
+"""
+Creates a csv with the SWE average for each month
+input:
+    timestrings {list} a list of strings in MM/DD/YYYY format. One for each time dimension in the input netCDF4
+    swe_sums {list} a list of swe_sums derived from the water_storage_timeseries function
+    output_file {str} file string of the output file. It should have already been tested before input to this function
+"""
+def create_csv(timestrings, swe_sums, output_csv):
+    print('Creating CSV and writing average SWE to file')
+    
+    # only write if we have the same number of swe sums and timestrings
+    if(len(timestrings) == len(swe_sums)):
+
+        # Open csv at the location that was given in the args
+        with open(output_csv, 'wb') as csvfile:
+
+            # create a csv writer
+            csvwriter = csv.writer(csvfile, dialect='excel')
+
+            # write the headers
+            csvwriter.writerow(['Month', 'SWE Sum'])
+
+            # loop through the results and write to the csv file
+            for i in range(len(timestrings)):
+                csvwriter.writerow([timestrings[i], swe_sums[i]])
+
+    else:
+        # errors out if they aren't the same length
+        print('ERROR: Timestrings length is not the same as SWE sums')
+        print('Timestrings length: ', len(timestrings))
+        print('SWE Sums length: ', len(swe_sums))
+
+
+"""
+Retrieves the fillvalue from the original input netCDF4 file. If there is no fill value specified,
+then it returns the default
+
+input:
+    input_netCDF4 {netCDF4.Dataseet} Dataset object from the netCDF4 library
+
+returns:
+    the fillvalue to be used when creating the netCDF4 output
+"""
+def get_fillvalue(input_netCDF4):
+    fillvalue = netCDF4.default_fillvals['f4']
+    if 'RUNSF' in input_netCDF4.variables:
+        runsf = input_netCDF4.variables['RUNSF']
+        if '_FillValue' in runsf.ncattrs():
+            fillvalue = runsf._FillValue
+            print('The fill value for RUNSF is: ', str(fillvalue))
+        else:
+            fillvalue = None
+
+    return fillvalue
+
+
+"""
+Creates an output netCDF4 file with...
+
+input:
+    input_filepath {str} a filepath given as one of the arguments to run the script. The location of the input nc4 file
+    output_filepath {str} a filepath given as one of the arguments to run the script. The location of the output nc4 file
+    fillvalue {str} the fillvalue to use when creating the masked arrays. Retrieve using the get_fillvalue function defined earlier
+    longitudes {list} longitudes taken from the original input nc4 file
+    latitudes {list}  latitudes taken from the original input nc4 file
+    swe_average {list} list containing SWE Averages calculated over the total surface area for a particular grid cell
+    timeseries {list} a list containing the total swe for a particular time (month in this case)
+"""
+def create_output_netCDF4(input_filepath, output_filepath, fillvalue, longitudes, latitudes, swe_averages, timeseries):
+    print('creating output netCDF4 file...')
+
+    # create the output nc4 file from the filepath supplied in the args
+    output_netCDF4 = netCDF4.Dataset(output_filepath, 'w'. format='NETCDF3_CLASSIC')
+    input_netCDF4 = netCDF4.Dataset(input_filepath, 'r')
+
+    # create dimensions
+    create_dimensions(output_netCDF4, longitudes, latitudes)
+
+    # create the variables then return them to use later in creating variable attributes
+    variables = create_variables(output_netCDF4, fillvalue)
+
+    # create global attributes for the nc4 file
+    create_global_attributes(input_filepath, output_netCDF4)
+
+    # create variable attributes from the original nc4 file
+    copy_variable_attributes(input_netCDF4, output_netCDF4)
+
+    # modify the CRS variable attributes (no idea why this is here)
+    modify_crs_attribute(output_netCDF4, variables)
+
+    # populate static data
+    print('populating static data')
+    variables['lon'][:] = input_netCDF4.variables['lon'][:]
+    variables['lat'][:] = input_netCDF4.variables['lat'][:] 
+
+    populate_dynamic_data(output_netCDF4, input_netCDF4, longitudes, latitudes, swe_averages, timeseries)
+
+    # close files
+    output_netCDF4.close()
+    input_netCDF4.close()
+
+
+"""
+creates the dimensions for the output netCDF4 file. 
+
+input:
+    output_netCDF4 {netCDF4.Dataset} the dataset which was created
+    longitudes {list} longitudes taken from the original input nc4 file
+    latitudes {list}  latitudes taken from the original input nc4 file
+"""
+def create_dimensions(output_netCDF4, longitudes, latitudes):
+    print('creating dimensions...')
+    time = output_netCDF4.createDimension('time', None)
+    lat = output_netCDF4.createDimension('lat', len(latitudes))
+    lon = output_netCDF4.createDimension('lon', len(longitudes))
+    nv = output_netCDF4.createDimension('nv', 2)  # no idea why this is here
+
+
+"""
+Creates the variables for the output nc4 file
+input:
+    output_netCDF4 {netCDF4.Dataset} the dataset which was created
+    fillvalue {str} the fillvalue to be used in the swe variable
+
+returns:
+    {dict} the new variables objects of the output dataset in {'name': variable} format
+"""
+def create_variables(output_netCDF4, fillvalue):
+    print('creating variables')
+    time = output_netCDF4.createVariable('time', 'i4', ('time',))
+    time_bands = output_netCDF4.createVariable('time_bands', 'i4', ('time', 'nv'))
+    lat = output_netCDF4.createVariable('lat', 'f4', ('lat',))
+    lon = output_netCDF4.createVariable('lon', 'f4', ('lon',))
+    swe = output_netCDF4.createVariable('swe', 'f4', ('time', 'lat', 'lon'), fill_value=fillvalue)
+
+    # not sure what crs is
+    crs = output_netCDF4.createVariable('crs', 'i4')
+
+    return {
+        'time': time,
+        'time_bands': time_bands,
+        'lat': lat,
+        'lon': lon,
+        'swe': swe,
+        'crs': crs
+    }
+
+
+"""
+Creates the global variables for the output nc4 file
+input:
+    output_netCDF4 {netCDF4.Dataset} the dataset which was created
+"""
+def create_global_attributes(input_filepath, output_netCDF4):
+    # Get current time without microseconds
+    dt = datetime.datetime.utcnow()
+    dt = dt.replace(microsecond=0)
+
+    # grab the current version of shbaam
+    version = subprocess.Popen('bash ../version.sh', stdout=subprocess.PIPE, shell=True).communicate()
+    version = version[0].rstrip()
+
+    # create global attributes
+    output_netCDF4.Conventions = 'CF-1.6'
+    output_netCDF4.title = ''
+    output_netCDF4.institution = ''
+    output_netCDF4.source = 'SHBAAM: ' + version + 'GRACE: ' + os.path.basename(input_filepath)
+    output_netCDF4.history = 'date created: ' + dt.isoformat() + '+00:00'
+    output_netCDF4.references = 'https://github.com/c-h-david/shbaam/'
+    output_netCDF4.comment = ''
+    output_netCDF4.featureType = 'timeSeries'
+
+
+"""
+Copies the attributes from the input netCDF4 file provided in the program args over to
+the newly created output file. 
+
+input: 
+    input_filepath {str} the filepath to the input nc4 file
+    destination {netCDF4.Dataset} the dataset which was created
+"""
+def copy_variable_attributes(source, destination):
+
+    print('Copying variable attributes...')
+
+    # copy over every variable attribute in the source attributes
+    for name, variable in source.variables.items():
+        var = destination.variables[name]
+        destination[name].setncatts(source[name].__dict__)
+
+
+"""
+These are for the WGS84 spheroid
+
+input:
+    output_netCDF4 {netCDF4.Dataset} the dataset which was created
+    output_variables {dict} the variables objects of the output dataset in {'name': variable} format
+"""
+def modify_crs_attribute(output_netCDF4, output_variables):
+    output_variables['swe'].grid_mappings = 'crs'
+    output_variables['crs'].grid_mapping_name = 'latitude_longitude'
+    output_variables['crs'].semi_major_axis = '6378137'
+    output_variables['crs'].inverse_flattening = '298.257223563' 
+
+
+"""
+populates the output file with the relavent data
+"""
+def populate_dynamic_data(output_netCDF4, input_netCDF4, longitudes, latitudes, swe_averages, timeseries):
+    print('populate dynamic data...')
+
+    for grid_cell in range(len(swe_averages)):
+        lon = longitudes[grid_cell]
+        lat = latitudes[grid_cell]
+        swe_average = swe_averages[grid_cell]
+        
+        for time in range(len(timeseries)):
+            output_netCDF4.variables['swe'][time, lat, lon] = input_netCDF4.variables['SWE'][time, lat, lon] - swe_average
+
+    # why do we do this?
+    output_netCDF4.variables['time'][:] = output_netCDF4.variables['time'][:] 
